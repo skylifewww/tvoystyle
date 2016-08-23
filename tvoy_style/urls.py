0ce5de50b95fe3433b20e75c7f7b3392e2c2c7f5
skylifewww <skylife@ukr.net>
2016-08-23 23:42:50 +0300

    patterns comment

--- tvoy_style/urls.py
+++ tvoy_style/urls.py
@@ -17,13 +17,13 @@ urlpatterns = [
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
-if settings.DEVELOPMENT and settings.DEBUG and 'debug_toolbar' in settings.INSTALLED_APPS:
-    import debug_toolbar
-    urlpatterns += patterns(
-        '',
-        url(r'^__debug__/', include(debug_toolbar.urls)))
+# if settings.DEVELOPMENT and settings.DEBUG and 'debug_toolbar' in settings.INSTALLED_APPS:
+#     import debug_toolbar
+#     urlpatterns += patterns(
+#         '',
+#         url(r'^__debug__/', include(debug_toolbar.urls)))
 
-if settings.DEVELOPMENT or settings.DEBUG:
-    urlpatterns += patterns(
-        'django.views.static',
-        url(r'^media/(?P<path>.*)$', 'serve', {'document_root': settings.MEDIA_ROOT}))
+# if settings.DEVELOPMENT or settings.DEBUG:
+#     urlpatterns += patterns(
+#         'django.views.static',
+#         url(r'^media/(?P<path>.*)$', 'serve', {'document_root': settings.MEDIA_ROOT}))
