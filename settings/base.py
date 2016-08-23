1cc3f706dc26187334ef7326cd5c1652ff1a0166
skylifewww <skylife@ukr.net>
2016-08-23 23:53:37 +0300

    allowed hosts

--- settings/base.py
+++ settings/base.py
@@ -11,7 +11,7 @@ DEBUG = False
 DOMAIN = 'localhost:8000'
 
 APPEND_SLASH = True
-ALLOWED_HOSTS = [DOMAIN]
+ALLOWED_HOSTS = ["tvoystyle.herokuapp.com"]
 
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
