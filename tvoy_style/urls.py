150d100b59b7d87cf47b5d4ef35d0525fa58bbc5
skylifewww <skylife@ukr.net>
2016-08-25 01:11:50 +0300

    contact form

--- tvoy_style/urls.py
+++ tvoy_style/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
-# from django.conf.urls import patterns, include, url
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
+# from django.conf.urls import include, url
 from tvoy_style.views import *
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -17,13 +17,13 @@ urlpatterns = [
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
-# if settings.DEVELOPMENT and settings.DEBUG and 'debug_toolbar' in settings.INSTALLED_APPS:
-#     import debug_toolbar
-#     urlpatterns += patterns(
-#         '',
-#         url(r'^__debug__/', include(debug_toolbar.urls)))
+if settings.DEVELOPMENT and settings.DEBUG and 'debug_toolbar' in settings.INSTALLED_APPS:
+    import debug_toolbar
+    urlpatterns += patterns(
+        '',
+        url(r'^__debug__/', include(debug_toolbar.urls)))
 
-# if settings.DEVELOPMENT or settings.DEBUG:
-#     urlpatterns += patterns(
-#         'django.views.static',
-#         url(r'^media/(?P<path>.*)$', 'serve', {'document_root': settings.MEDIA_ROOT}))
+if settings.DEVELOPMENT or settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        url(r'^media/(?P<path>.*)$', 'serve', {'document_root': settings.MEDIA_ROOT}))
--- tvoy_style/views.py
+++ tvoy_style/views.py
@@ -13,7 +13,10 @@ from django.template import Context
 # from collection.forms import ContactForm
 
 # add to your views
+ 
+ 
 def contact(request):
+
     form_class = ContactForm
 
     # new logic!
@@ -40,7 +43,7 @@ def contact(request):
             content = template.render(context)
 
             email = EmailMessage(
-                "New contact form submission",
+                "Пришел новый заказ",
                 content,
                 "Your website" +'',
                 ['skylifewww@gmail.com'],
@@ -59,10 +62,45 @@ def contact(request):
 
 
 def index(request):
+    form_class = ContactForm
 
     # return_path_f(request)
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
 
-    return render_to_response("index.html")
+        if form.is_valid():
+            contact_name = request.POST.get(
+                'contact_name'
+            , '')
+            contact_email = request.POST.get(
+                'contact_email'
+            , '')
+            form_content = request.POST.get('content', '')
+
+            # Email the profile with the 
+            # contact information
+            template = get_template('contact_template.txt')
+            context = Context({
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'form_content': form_content,
+            })
+            content = template.render(context)
+
+            email = EmailMessage(
+                "Задан новый вопрос",
+                content,
+                "Your website" +'',
+                ['skylifewww@gmail.com'],
+                headers = {'Reply-To': contact_email }
+            )
+            email.send()
+            return redirect('/') 
+    
+
+    return render(request, 'index.html', {
+        'form': form_class,
+    })
 
 
 
